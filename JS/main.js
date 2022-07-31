5eff69f14cfaaaa8e5b720f840b8fed0ef92af86
mostafamhli23 <100767886+mostafamhli23@users.noreply.github.com>
2022-07-31 17:20:15 +0300

    version 1.1

--- JS/main.js
+++ JS/main.js
@@ -1,29 +1,6 @@
-
 document.querySelector(".control-buttons span").onclick = function () {
-    (async () => {
-    await Swal.fire({
-        title: "Enter Your Name",
-        html: '<input id="swal-input1" class="swal2-input">',
-        focusConfirm: false,
-        preConfirm: () => {
-        return [
-            document.getElementById("swal-input1").value,
-            localStorage.setItem(
-            "username",
-            document.getElementById("swal-input1").value
-            ),
-        ];
-        },
-        allowOutsideClick: () => !Swal.isLoading(),
-    }).then((result) => {
-        if (result.isConfirmed) {
-        document.querySelector(".name span").innerHTML =
-            localStorage.getItem("username");
-        }
-    });
-    })();
-    document.querySelector(".control-buttons").remove();
-    
+    startP();
+  document.querySelector(".control-buttons").remove();
 };
 
 let duration = 1000;
@@ -31,85 +8,92 @@ let blocksContainer = document.querySelector(".memory-game-blocks");
 let blocks = Array.from(blocksContainer.children);
 let orderRange = [...Array(blocks.length).keys()];
 orderRange = shuffle(orderRange);
-blocksContainer.classList.add('no-clicking')
-blocks.forEach((block, index) => {
-    block.style.order = orderRange[index];
-    block.children[0].style.cssText = 'transform:rotateY(360deg);'
-    block.children[1].style.cssText = 'transform:rotateY(360deg);'
-
-    setTimeout(() => {
-        block.children[0].style.cssText = 'transform:rotateY(-360deg);'
-        block.children[1].style.cssText = 'transform:rotateY(-180deg);'
-        blocksContainer.classList.remove('no-clicking')
-    },10000)
-
-    //block.classList.remove('front')
-    block.addEventListener("click", () => {
-    flipBlock(block);
-    });
-});
-
+blocksContainer.classList.add("no-clicking");
+function startP(){
+    blocks.forEach((block, index) => {
+        block.style.order = orderRange[index];
+        block.children[0].style.cssText = "transform:rotateY(360deg);";
+        block.children[1].style.cssText = "transform:rotateY(360deg);";
+      
+          setTimeout(() => {
+            block.children[0].style.cssText = "transform:rotateY(-360deg);";
+            block.children[1].style.cssText = "transform:rotateY(-180deg);";
+            blocksContainer.classList.remove("no-clicking");
+          }, 10000);
+      
+        //block.classList.remove('front')
+        block.addEventListener("click", () => {
+          flipBlock(block);
+        });
+      });
+}
 
 function flipBlock(selectedBlock) {
-    selectedBlock.classList.add("is-flipped");
+  selectedBlock.classList.add("is-flipped");
 
-    let allFlippedBlock = blocks.filter((flippedBlock) =>
+  let allFlippedBlock = blocks.filter((flippedBlock) =>
     flippedBlock.classList.contains("is-flipped")
-    );
+  );
 
-    if (allFlippedBlock.length === 2) {
+  if (allFlippedBlock.length === 2) {
     noClicking();
     checkMatchedBlock(allFlippedBlock[0], allFlippedBlock[1]);
-    }
+  }
 
-    if(diditWon()===true) {
-        Swal.fire({
-            position: 'center-center',
-            icon: 'success',
-            title: 'Congratulations',
-            showConfirmButton: false,
-            timer: 2500
-            })
-    }
-    
+  if (diditWon() === true) {
+    let congrat = document.querySelector(".Congrat");
+    congrat.style.display = "block";
+    document.querySelector(".Congrat .butt .yes").onclick = function () {
+      location.reload();
+    };
+    document.querySelector(".Congrat .butt .no").onclick = function () {
+      window.close();
+    };
+  }
 }
 
 function noClicking() {
-    blocksContainer.classList.add("no-clicking");
-    setTimeout(() => {
+  blocksContainer.classList.add("no-clicking");
+  setTimeout(() => {
     blocksContainer.classList.remove("no-clicking");
-    }, duration);
+  }, duration);
 }
 
 function checkMatchedBlock(firstBlock, secondBlock) {
-    let triesElement = document.querySelector(".tries span");
+  let triesElement = document.querySelector(".tries span");
 
-    if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
     firstBlock.classList.remove("is-flipped");
     secondBlock.classList.remove("is-flipped");
 
     firstBlock.classList.add("has-match");
     secondBlock.classList.add("has-match");
     document.getElementById("success").play();
-    } else {
+  } else {
     triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
-    if(triesElement.innerHTML > 5){
-        JSalert()
+    if (triesElement.innerHTML >= 5) {
+      let gameOver = document.querySelector(".game-over");
+      gameOver.style.display = "block";
+      document.querySelector(".game-over .butt .yes").onclick = function () {
+        location.reload();
+      };
+      document.querySelector(".game-over .butt .no").onclick = function () {
+        window.close();
+      };
     }
     setTimeout(() => {
-        firstBlock.classList.remove("is-flipped");
-        secondBlock.classList.remove("is-flipped");
+      firstBlock.classList.remove("is-flipped");
+      secondBlock.classList.remove("is-flipped");
     }, duration);
     document.getElementById("fail").play();
-    }
+  }
 }
 
-
 function shuffle(array) {
-    let current = array.length,
+  let current = array.length,
     temp,
     random;
-    while (current > 0) {
+  while (current > 0) {
     random = Math.floor(Math.random() * current);
     current--;
     //[1] Save Current element in stach
@@ -118,33 +102,16 @@ function shuffle(array) {
     array[current] = array[random];
     //[3] random element = get element from stach
     array[random] = temp;
-    }
-    return array;
+  }
+  return array;
 }
 
 function diditWon() {
-    didHeWon = true;
-    blocks.forEach((block)=>{
-        if(! block.classList.contains('has-match')){
-            didHeWon = false;
-        }
-    })
-    return didHeWon;
-}
-
-function JSalert(){
-    (async () => {
-        await Swal.fire({
-        title: 'Do you want to try again!?',
-        showDenyButton: true,
-        confirmButtonText: 'Yes',
-        denyButtonText: `No`,
-        }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.reload();
-        } else if (result.isDenied) {
-            window.opener = self;
-            window.close();
-        }
-    })})()
+  didHeWon = true;
+  blocks.forEach((block) => {
+    if (!block.classList.contains("has-match")) {
+      didHeWon = false;
+    }
+  });
+  return didHeWon;
 }
